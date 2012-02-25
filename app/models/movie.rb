3c24859431c138fc17d890d0a4730cf83344c031
Case Walker <4casewalker@gmail.com>
2012-02-24 16:57:47 -0800

    model method added to get movies with the same director

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,11 @@ class Movie < ActiveRecord::Base
   def self.all_ratings
     %w(G PG PG-13 NC-17 R)
   end
+
+  def self.get_directors_movies(id)
+    director = Movie.find_by_id(id).director
+    Movie.where(%Q{
+        director = (?) and id not in (?)},
+        director, [id])
+  end
 end
